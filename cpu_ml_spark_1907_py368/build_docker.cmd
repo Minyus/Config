392e46197b1cd2994c5181cfc63c7c8108c7ba4e
Minyus <yusuke.minami86@gmail.com>
2019-07-11 01:09:17 +0800

    Remove Mesos

--- cpu_ml_spark_1907_py368/build_docker.cmd
+++ cpu_ml_spark_1907_py368/build_docker.cmd
@@ -1,3 +1,3 @@
-docker build -t minyus86/cpu_ml_spark_1907_py368:v0.0.4 .
-docker run -v /home:/home -p 22:22 -p 5000:5000 --name ml minyus86/cpu_ml_spark_1907_py368:v0.0.4
+docker build -t minyus86/cpu_ml_spark_1907_py368:v0.0.5 .
+docker run -v /home:/home -p 22:22 -p 5000:5000 -p 6006:6006 --name ml minyus86/cpu_ml_spark_1907_py368:v0.0.5
 pause
