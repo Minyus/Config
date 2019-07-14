169d30d21efe885cbe0e5ca20ed403a9b684642d
Minyus <yusuke.minami86@gmail.com>
2019-07-15 01:26:28 +0800

    Upgrade ml_spark to v0.0.6

--- cpu_ml_spark_1907_py368/build_docker.cmd
+++ cpu_ml_spark_1907_py368/build_docker.cmd
@@ -1,3 +1,2 @@
-docker build -t minyus86/cpu_ml_spark_1907_py368:v0.0.5 .
-docker run -v /home:/home -p 22:22 -p 5000:5000 -p 6006:6006 --name ml minyus86/cpu_ml_spark_1907_py368:v0.0.5
+docker build -t minyus86/cpu_ml_spark_1907_py368:v0.0.6 .
 pause
