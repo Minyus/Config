9a0d652f8526de5954c61817491ec928cb449ae9
Minyus <yusuke.minami86@gmail.com>
2020-05-06 03:23:49 +0000

    Add logging_config

---
+++ logging_config/logging_config.py
@@ -0,0 +1,79 @@
+from logging.config import dictConfig
+
+logging_config = {
+    "disable_existing_loggers": False,
+    "formatters": {
+        "simple": {
+            "format": "[%(asctime)s.%(msecs)03d|%(name)s|%(levelname)s] %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "simple",
+            "level": "INFO",
+            "stream": "ext://sys.stdout",
+        },
+        "debug_file_handler": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "level": "DEBUG",
+            "formatter": "simple",
+            "filename": "./debug.log",
+            "maxBytes": 10485760,  # 10MB
+            "backupCount": 20,
+            "encoding": "utf8",
+            "delay": True,
+        },
+        "info_file_handler": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "level": "INFO",
+            "formatter": "simple",
+            "filename": "./info.log",
+            "maxBytes": 10485760,  # 10MB
+            "backupCount": 20,
+            "encoding": "utf8",
+            "delay": True,
+        },
+        "error_file_handler": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "level": "ERROR",
+            "formatter": "simple",
+            "filename": "./errors.log",
+            "maxBytes": 10485760,  # 10MB
+            "backupCount": 20,
+            "encoding": "utf8",
+            "delay": True,
+        },
+    },
+    "loggers": {
+        "anyconfig": {
+            "handlers": [
+                "console",
+                "debug_file_handler",
+                "info_file_handler",
+                "error_file_handler",
+            ],
+            "level": "WARNING",
+            "propagate": False,
+        },
+    },
+    "root": {
+        "handlers": [
+            "console",
+            "debug_file_handler",
+            "info_file_handler",
+            "error_file_handler",
+        ],
+        "level": "DEBUG",
+    },
+    "version": 1,
+}
+
+dictConfig(logging_config)
+
+if __name__ == "__main__":
+    from logging import getLogger
+
+    log = getLogger(__name__)
+    log.info("# Logging configured.")
